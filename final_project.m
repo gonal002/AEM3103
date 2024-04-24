98b1accd8690f385bcb21ad2f124ba3c3cad2d36
R. Silva <gonal002@umn.edu>
2024-04-24 16:53:59 -0500

    Did question 2

--- final_project.m
+++ final_project.m
@@ -1,43 +1,70 @@
-% Paper Airplane Numerical Study
- %  Final Project: AEM 3103 Spring 2024
- 
- %  - By: <NAME or NAMES of team members, if completed as a team>
+	global CL CD S m g rho	
+	S		=	0.017;			% Reference Area, m^2
+	AR		=	0.86;			% Wing Aspect Ratio
+	e		=	0.9;			% Oswald Efficiency Factor;
+	m		=	0.003;			% Mass, kg
+	g		=	9.8;			% Gravitational acceleration, m/s^2
+	rho		=	1.225;			% Air density at Sea Level, kg/m^3	
+	CLa		=	3.141592 * AR/(1 + sqrt(1 + (AR / 2)^2));
+							% Lift-Coefficient Slope, per rad
+	CDo		=	0.02;			% Zero-Lift Drag Coefficient
+	epsilon	=	1 / (3.141592 * e * AR);% Induced Drag Factor	
+	CL		=	sqrt(CDo / epsilon);	% CL for Maximum Lift/Drag Ratio
+	CD		=	CDo + epsilon * CL^2;	% Corresponding CD
+	LDmax	=	CL / CD;			% Maximum Lift/Drag Ratio
+	Gam		=	-atan(1 / LDmax);	% Corresponding Flight Path Angle, rad
+	V		=	sqrt(2 * m * g /(rho * S * (CL * cos(Gam) - CD * sin(Gam))));
+							% Corresponding Velocity, m/s
+	Alpha	=	CL / CLa;			% Corresponding Angle of Attack, rad
+	
+%%	a) Equilibrium Glide at Maximum Lift/Drag Ratio -- Question 2
+	H		=	2;			% h_0 [m]
+	R		=	0;			% R_0 [m]
+	to		=	0;			% t_0 [sec]
+	tf		=	6;			% t_f [sec]
+	tspan	=	[to tf];
+    
+    % Same Flight path angle, varying velocity
+    Gam = -0.18;
+    Vn = 3.55; % Nominal
+	xoa		=	[Vn;Gam;H;R];
+	[ta,xa]	=	ode23('EqMotion',tspan,xoa);
 
- %  ## Summary of Findings
- %  <Show the variations studied in a table>
- 
- %  Summarized what was accomplished in this study.  Describe 2-4 observations from simulating the flight path.
- %  Reference the figures below as needed.
- 
- %  *If the analysis falls short of the goal, this is your chance to explain what was done or what were the barriers.*
- 
- %  # Code Listing
- %  A list of each function/script and a single-line description of what it does.  The name of the function/script should link to the file in the repository on GitHub.
- 
- %  # Figures
- 
- %  ## Fig. 1: Single Parameter Variation
- %  <2D trajectory simulated by varying single parameter at at time>
- %  <The above plot should also show the nominal trajectory>
- 
- %  Briefly describe what is being shown in the figure.
- 
- %  ## Fig. 2: Monte Carlo Simulation
- %  <2D trajectories simulated using random sampling of parameters, overlay polynomial fit onto plot.>
- 
- %  Briefly describe what is being shown in the figure.
- 
- % ## Fig. 3: Time Derivatives
- % <Time-derivative of height and range for the fitted trajectory>
- 
- %  Briefly describe what is being shown in the figure.
- 
- %  (Below are for teams of 2-3 people)
- 
- %  # Animation
- %  ## Point-Mass Animation
- %  <Animated GIF showing 2D trajectory for nominal and the scenario (V=7.5 m/s, Gam=+0.4 rad)>
- 
- %  (Below are for teams of 3 people)
- %  ## Graphical Animation
- %  <Same as the above animation, except that the moving *point* should be a 2D drawing of an airplane, drawn using CAD>
+    Vl = 2; % Lower
+    xob		=	[Vl;Gam;H;R];
+	[tb,xb]	=	ode23('EqMotion',tspan,xob);
+
+    Vh = 7.5; % Higher
+    xoc		=	[Vh;Gam;H;R];
+	[tc,xc]	=	ode23('EqMotion',tspan,xoc);
+
+    % Same Velocity, varying Flight Path Angle
+    Gam_n = -0.18; % Nominal
+	xod		=	[Vn;Gam_n;H;R];
+	[td,xd]	=	ode23('EqMotion',tspan,xod);
+
+    Gam_l = -0.5; % Lower
+    xoe		=	[Vn;Gam_l;H;R];
+	[te,xe]	=	ode23('EqMotion',tspan,xoe);
+
+    Gam_h = 0.4; % Higher
+    xog		=	[Vn;Gam_h;H;R];
+	[tg,xg]	=	ode23('EqMotion',tspan,xog);
+    
+    % Plot 2
+    figure
+	subplot(2,1,1)
+    plot(xa(:,4),xa(:,3),xb(:,4),xb(:,3),xc(:,4),xc(:,3))
+    xlabel('Range, m'), ylabel('Height, m'), grid
+    title('Varying Velocity', 'FontSize', 12);
+    %% Change colors of different velocities
+
+    subplot(2,1,2)
+    plot(xd(:,4),xd(:,3),xe(:,4),xe(:,3),xg(:,4),xg(:,3))
+    xlabel('Range, m'), ylabel('Height, m'), grid
+    title('Varying Flight Path Angle', 'FontSize', 12);
+    %% Change colors of different velocities
+
+    %% Question 3
+    
+
